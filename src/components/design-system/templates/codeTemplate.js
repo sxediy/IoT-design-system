055843331bd85861ecee9605f447793a65c096a5
admin <admin@MacBook-Pro-admin.local>
2019-01-11 15:22:16 +0300

    Добавил инлайн стиль background color для библиотеки раскраски кода

--- src/components/design-system/templates/codeTemplate.js
+++ src/components/design-system/templates/codeTemplate.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import codeStyle from 'design-system/templates/codeStyle.less';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/styles/hljs';
+import { githubGist } from 'react-syntax-highlighter/dist/styles/hljs';
+
+// import { dark } from 'react-syntax-highlighter/dist/styles/prism';
+// список цветовых тем https://github.com/conorhastings/react-syntax-highlighter/blob/HEAD/AVAILABLE_STYLES_HLJS.MD
 
 const codeTemplate = (code) =>
   <div className={codeStyle.container}>
-    <SyntaxHighlighter language='javascript' style={docco}>{code}</SyntaxHighlighter>
+    <SyntaxHighlighter
+      language='javascript'
+      style={githubGist}
+      customStyle={{ background: '#fafafa' }}
+    >
+      {code}
+    </SyntaxHighlighter>
   </div >;
 
 export default codeTemplate;
