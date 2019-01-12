c210de21b9b6a2694eb23d300a47fb9d377989a0
admin <admin@MacBook-Pro-admin.local>
2019-01-12 17:51:13 +0300

    Убрал роутер

--- src/app.js
+++ src/app.js
@@ -1,32 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { routes } from './routes';
+import Home from 'pages//home/home';
 
-const App = () => {
-  const renderSwitch = () => (
-    <Switch>
-      {routes.map(route => {
-        const component = route.page;
-        return (
-          <Route
-            key={route.path}
-            exact={route.isExact}
-            path={route.path}
-            component={component}
-          />
-        );
-      })}
-      <Redirect from='*' to='/' />
-    </Switch>
-  );
 
-  return (
-    <Router>
-      <React.Fragment>
-        {renderSwitch()}
-      </React.Fragment>
-    </Router>
-  );
-};
+const App = () => <Home />;
+
 
 export default App;
