1dd71fed72bb08ac112a88dc400f92fcb8f661a5
admin <admin@MacBook-Pro-admin.local>
2019-01-23 15:02:39 +0300

    мелочи

--- src/components/menuBar.js
+++ src/components/menuBar.js
@@ -27,21 +27,21 @@ class MenuBar extends React.Component {
         <SubMenu key="basic" title="Базовые компоненты">
           <Menu.Item key="resourcesTabs">Табы ресурсов</Menu.Item>
           <Menu.Item key="counterTabs">Табы счётчиков</Menu.Item>
-          <Menu.Item key="pricingTableCards">Карточки ресурсов</Menu.Item>
           <Menu.Item key="resourceNameAndUnit">Ресурсы</Menu.Item>
-          <Menu.Item key="appHeaderNavBar">Верхняя панель навигации</Menu.Item>
           <Menu.Item key="appHeaderBell">Звоночек</Menu.Item>
-          <Menu.Item key="appHeaderProfile">Иконка профиля с дропдауном</Menu.Item>
           <Menu.Item key="notifications">Уведомления всплывающие</Menu.Item>
         </SubMenu>
         <SubMenu key="universal" title="Уникальные компоненты">
           <Menu.Item key="pricingTable">Таблица ресурсов</Menu.Item>
+          <Menu.Item key="pricingTableCards">Карточки ресурсов</Menu.Item>
           <Menu.Item key="appHeader">Верхняя панель приложения</Menu.Item>
+          <Menu.Item key="appHeaderNavBar">Верхняя панель навигации</Menu.Item>
+          <Menu.Item key="appHeaderProfile">Иконка профиля с дропдауном</Menu.Item>
           <Menu.Item key="rowTimeFilter">Фильтр времени</Menu.Item>
           <Menu.Item key="buttonWithDoubleMenu">Кнопка с двойным выпадающим списком</Menu.Item>
           <Menu.Item key="buttonWithTripleMenu">Кнопка с тройным выпадающим списком</Menu.Item>
           <Menu.Item key="smartCheckboxWithButton">Чекбокс с кнопкой</Menu.Item>
-          <Menu.Item key="treeSelector">Селектор в виде дерева</Menu.Item>
+          <Menu.Item key="treeSelector">Селектор с древовидной структурой</Menu.Item>
         </SubMenu>
       </Menu>
     );
