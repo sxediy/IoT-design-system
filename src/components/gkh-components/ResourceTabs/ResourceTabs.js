53ece2024bab20680d67c8927ef0e4e28c46b54b
admin <admin@MacBook-Pro-admin.local>
2019-01-24 13:16:39 +0300

    Добавил в 'Табы ресурсов' обработку клика с помощью useState из React Hooks

--- src/components/gkh-components/ResourceTabs/ResourceTabs.js
+++ src/components/gkh-components/ResourceTabs/ResourceTabs.js
@@ -1,35 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import styles from 'components/gkh-components/ResourceTabs/ResourceTabs.less';
 
 
-const ResourceTabs = ({
-  tabsArr,
-  initActiveElement,
-  onClick,
-}) =>
-  <div className={styles.gkhTabs}>
-    {
-      tabsArr.map(tab =>
-        <div
-          key={ tab.typeName }
-          className={classnames(styles.tab, styles[tab.eng], { [styles.active]: initActiveElement === tab.typeName })}
-          onClick={ onClick }
-        >
-          <div className={styles.icon} />
-          <div>
-            { tab.title }
+const ResourceTabs = ({ tabsArr, initActiveElement }) => {
+  const [currentActive, setNewActive] = useState(initActiveElement);
+
+  return (
+    <div className={styles.gkhTabs}>
+      {
+        tabsArr.map(tab =>
+          <div
+            key={ tab.typeName }
+            className={classnames(styles.tab, styles[tab.eng], { [styles.active]: currentActive === tab.typeName })}
+            onClick={ () => setNewActive(tab.typeName) }
+          >
+            <div className={styles.icon} />
+            <div>
+              { tab.title }
+            </div>
           </div>
-        </div>
-      )
-    }
-  </div>;
+        )
+      }
+    </div>
+  );
+};
 
 ResourceTabs.propTypes = {
   tabsArr: PropTypes.array,
   initActiveElement: PropTypes.string,
-  onClick: PropTypes.func,
 };
 
 export { ResourceTabs };
