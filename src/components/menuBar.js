13f19b8c97e98e3b0fd6ec9518b80ea880fd9fc1
admin <admin@MacBook-Pro-admin.local>
2019-01-25 15:40:22 +0300

    Рефакторинг. Сделал menuBar функциональным компонентом

--- src/components/menuBar.js
+++ src/components/menuBar.js
@@ -5,50 +5,46 @@ import { connect } from 'react-redux';
 
 const { SubMenu } = Menu;
 
-class MenuBar extends React.Component {
-  static propTypes = {
-    changeUIContainer: PropTypes.func,
-    activeNameOfComponent: PropTypes.string,
-  };
-
-  handleClick = (e) => {
+const MenuBar = ({ activeNameOfComponent, changeUIContainer }) => {
+  const handleClick = (e) => {
     console.log('clickOnMenuBar ', e);
-    this.props.changeUIContainer(e.key);
-  }
+    changeUIContainer(e.key);
+  };
+  return (
+    <Menu
+      onClick={handleClick}
+      style={{ flexGrow: 1 }}
+      defaultSelectedKeys={[activeNameOfComponent]}
+      defaultOpenKeys={['universal']}
+      mode="inline"
+    >
+      <SubMenu key="basic" title="Базовые компоненты">
+        <Menu.Item key="resourcesTabs">Табы ресурсов</Menu.Item>
+        <Menu.Item key="counterTabs">Табы счётчиков</Menu.Item>
+        <Menu.Item key="resourceNameAndUnit">Ресурсы</Menu.Item>
+        <Menu.Item key="appHeaderBell">Звоночек</Menu.Item>
+        <Menu.Item key="notifications">Уведомления всплывающие</Menu.Item>
+      </SubMenu>
+      <SubMenu key="universal" title="Уникальные компоненты">
+        <Menu.Item key="pricingTable">Таблица ресурсов</Menu.Item>
+        <Menu.Item key="pricingTableCards">Карточки ресурсов</Menu.Item>
+        <Menu.Item key="appHeader">Верхняя панель приложения</Menu.Item>
+        <Menu.Item key="appHeaderNavBar">Верхняя панель навигации</Menu.Item>
+        <Menu.Item key="appHeaderProfile">Иконка профиля с дропдауном</Menu.Item>
+        <Menu.Item key="rowTimeFilter">Фильтр времени</Menu.Item>
+        <Menu.Item key="buttonWithDoubleMenu">Кнопка с двойным выпадающим списком</Menu.Item>
+        <Menu.Item key="buttonWithTripleMenu">Кнопка с тройным выпадающим списком</Menu.Item>
+        <Menu.Item key="smartCheckboxWithButton">Чекбокс с кнопкой</Menu.Item>
+        <Menu.Item key="treeSelector">Селектор с древовидной структурой</Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+};
 
-  render() {
-    const { activeNameOfComponent } = this.props;
-    return (
-      <Menu
-        onClick={this.handleClick}
-        style={{ flexGrow: 1 }}
-        defaultSelectedKeys={[activeNameOfComponent]}
-        defaultOpenKeys={['universal']}
-        mode="inline"
-      >
-        <SubMenu key="basic" title="Базовые компоненты">
-          <Menu.Item key="resourcesTabs">Табы ресурсов</Menu.Item>
-          <Menu.Item key="counterTabs">Табы счётчиков</Menu.Item>
-          <Menu.Item key="resourceNameAndUnit">Ресурсы</Menu.Item>
-          <Menu.Item key="appHeaderBell">Звоночек</Menu.Item>
-          <Menu.Item key="notifications">Уведомления всплывающие</Menu.Item>
-        </SubMenu>
-        <SubMenu key="universal" title="Уникальные компоненты">
-          <Menu.Item key="pricingTable">Таблица ресурсов</Menu.Item>
-          <Menu.Item key="pricingTableCards">Карточки ресурсов</Menu.Item>
-          <Menu.Item key="appHeader">Верхняя панель приложения</Menu.Item>
-          <Menu.Item key="appHeaderNavBar">Верхняя панель навигации</Menu.Item>
-          <Menu.Item key="appHeaderProfile">Иконка профиля с дропдауном</Menu.Item>
-          <Menu.Item key="rowTimeFilter">Фильтр времени</Menu.Item>
-          <Menu.Item key="buttonWithDoubleMenu">Кнопка с двойным выпадающим списком</Menu.Item>
-          <Menu.Item key="buttonWithTripleMenu">Кнопка с тройным выпадающим списком</Menu.Item>
-          <Menu.Item key="smartCheckboxWithButton">Чекбокс с кнопкой</Menu.Item>
-          <Menu.Item key="treeSelector">Селектор с древовидной структурой</Menu.Item>
-        </SubMenu>
-      </Menu>
-    );
-  }
-}
+MenuBar.propTypes = {
+  changeUIContainer: PropTypes.func,
+  activeNameOfComponent: PropTypes.string,
+};
 
 
 const mapDispatchToProps = (dispatch) => (
@@ -58,4 +54,6 @@ const mapDispatchToProps = (dispatch) => (
   }
 );
 
+
 export default connect(null, mapDispatchToProps)(MenuBar);
+
